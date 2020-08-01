7f89f7a22ffd6d814640db732960282e59d14ba6
Ben Hilliard <benhilliard23@hotmail.com>
2020-08-02 00:53:04 +0930

    commit with updated server.js however not yet finalised

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,8 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
-
+var fs = require("fs");
+var { v4: uuidv4 } = require('uuid');
 
 // Sets up the Express App
 // =============================================================
@@ -14,43 +15,45 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// Arrays for Notes
-// =============================================================
-var notes = [
-    {
-        title: "Greeting",
-        text: "Hello WOrld"  
-    }
-];
+// Routes 
 
-// Routes
-// =============================================================
+// Routes to send user to correct pages in browser
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+    return res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
+    return res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
+//Routes to handle 
+
 app.get("/api/notes", function (req, res) {
-    return res.json(notes);
+    res.sendFile(path.join(__dirname, "./db/db.json"))
+
 });
 
 app.post("/api/notes", function (req, res) {
-    var newNote = req.body;
-
-    console.log(newNote);
-
-    notes.push(newNote);
-
-    res.json(newNote);
+    var newNote = {
+        id: uuidv4(),
+        title: req.body.title,
+        text: req.body.text
+    }
+    let prevNote = JSON.parse(fs.readFileSync(path.join(__dirname, "./db/db.json"), "utf-8"))
+    //console.log(newNote);
+    prevNote.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(prevNote));
+    res.json(prevNote);
 });
 
-app.delete("/api/notes", function (req, res) {
-    notes = [];
-    console.log("test");
+app.delete("/api/notes/:id", function (req, res) {
+    var noteId = req.params.id;
+    //console.log(uniqueId);
+    var noteList = JSON.parse(fs.readFileSync(path.join(__dirname, "./db/db.json"), "utf-8"))
+    var newNotes = noteList.filter(noteList =>  noteList.id != noteId );
+    fs.writeFileSync("./db/db.json", JSON.stringify(newNotes));
+    res.send(newNotes);
 })
 
 
